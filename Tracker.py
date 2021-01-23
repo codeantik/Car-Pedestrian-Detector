fe3c43adb4b67ee3f3f99f6aa18e89c4d76cecc7
Ankit <ankit.sin099@gmail.com>
2021-01-23 23:12:30 +0530

    first commit

---
+++ Tracker.py
@@ -0,0 +1,62 @@
+from cv2 import cv2
+
+# img_file = 'car3_image.jpg'
+
+video = cv2.VideoCapture('ride_video.mp4')
+
+car_tracker_file = 'cars.xml'
+pedestrian_tracker_file = 'haarcascade_fullbody.xml'
+
+car_tracker = cv2.CascadeClassifier(car_tracker_file)
+pedestrian_tracker = cv2.CascadeClassifier(pedestrian_tracker_file)
+
+while True:
+
+    (read_successfull, frame) = video.read()
+
+    if read_successfull:
+        grayscaled_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    else:
+        break
+
+    cars = car_tracker.detectMultiScale(grayscaled_frame)
+    pedestrian = pedestrian_tracker.detectMultiScale(grayscaled_frame)
+
+    for (x, y, w, h) in cars:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+    for (x, y, w, h) in pedestrian:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 255), 2)
+
+    cv2.imshow('Car & Pedestrian Tracker', frame)
+
+    key = cv2.waitKey(1)
+
+    if key == 81 or key == 113:
+        break
+
+video.release()
+
+print("Completed")
+
+'''
+
+img = cv2.imread(img_file)
+
+black_n_white = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+car_tracker = cv2.CascadeClassifier(classifier_file)
+
+cars = car_tracker.detectMultiScale(black_n_white)
+
+print(cars)
+
+for (x, y, w, h) in cars:
+    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+cv2.imshow('Car & Pedestrian Detector', img)
+
+cv2.waitKey()
+
+
+'''
